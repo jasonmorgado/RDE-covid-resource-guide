31f6b2bc61534997baa0fef31b17d3285b903c5c
shail7777 <parekhshail7@gmail.com>
2021-11-16 17:22:38 -0500

    added map componenet

--- covid-resource-guide/src/index.js
+++ covid-resource-guide/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import './index.css';
 import App from './App';
+import Map from './Map';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Map />
   </React.StrictMode>,
   document.getElementById('root')
 );
