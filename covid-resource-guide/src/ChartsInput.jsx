ac7a28fe2e64c3d8384472d77716a827cc746f98
Jason Morgado <jasonsanimations@gmail.com>
2021-11-17 10:05:38 -0500

    Added user input elements for charts page

--- covid-resource-guide/src/ChartsInput.jsx
+++ covid-resource-guide/src/ChartsInput.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
 import DatePicker from 'react-date-picker';
 import { Multiselect } from 'multiselect-react-dropdown';
+import Dropdown from 'react-dropdown';
 
 function ChartsInput(){
   // TODO County Dropdown
   // TODO Metric Dropdown
   // TODO Date Selectors
+
+  // Multi-Select Dropdown: https://reactjsexample.com/react-multiselect-dropdown-with-search-and-various-options/
   var location_data = {
       county_list: [{name: 'Bergen', id: 1, state:"New Jersey"},{name: 'Essex', id: 2, state:"New Jersey"}, {name:"Albany", id:3, state:"New York"}]
   };
@@ -26,11 +29,28 @@ function ChartsInput(){
     console.log("Removed:"+removedItem.name);
   }
 
+
+
+  // Metric Dropdown
   function onSelectMetric(selectedList, selectedItem) {
     console.log("Selected:"+selectedItem.name);
   }
+
+  const metricOptions = [
+    "Daily Cases", "Total Cases", "Daily Deaths", "Total Deaths", "Daily Recoveries", "Total Recoveries"
+  ];
+  const defaultMetric = metricOptions[0];
+
+
+  // https://github.com/wojtekmaj/react-date-picker
+  const [startDate, onChangeStartDate] = useState(new Date());
+  const [endDate, onChangeEndDate] = useState(new Date());
+
+//<Dropdown options={metricOptions} onChange={onSelectMetric} value={defaultMetric} placeholder="Select a metric" />;
+
   return (
     <div id="ChartsInput">
+
     <Multiselect
       options={location_data.county_list} // Options to display in the dropdown
       selectedValues={location_data.selectedValue} // Preselected value to persist in dropdown
@@ -42,12 +62,26 @@ function ChartsInput(){
       closeOnSelect="false"
     />
     <Multiselect
-      options={metric_data.metric_options}
-      selectedValues={metric_data.selectedValue}
+      options={metric_data.metric_options} // Options to display in the dropdown
+      selectedValues={metric_data.selectedValue} // Preselected value to persist in dropdown
       onSelect={onSelectMetric} // Function will trigger on select event
       displayValue="name" // Property name to display in the dropdown options
+      showCheckbox="false"
+      closeOnSelect="true"
       singleSelect="true"
+
     />
+
+
+    <DatePicker
+      onChange={onChangeStartDate}
+      value={startDate}
+    />
+    <DatePicker
+      onChange={onChangeEndDate}
+      value={endDate}
+    />
+
     </div>
   );
 }
