1b9d080e047f350efe445f6e345e74d6fe8e74ae
shail7777 <parekhshail7@gmail.com>
2021-11-23 17:05:37 -0500

    Fixed the color on map and also disable rotations

--- covid-resource-guide/src/HeatMap.js
+++ covid-resource-guide/src/HeatMap.js
@@ -33,7 +33,7 @@ export default function App() {
 
   const [max, steMax] = useState(0);
 
-  const [startDate, setStartDate] = useState(new Date());
+  const [startDate, setStartDate] = useState(new Date("01 January 2020 12:00 UTC"));
   const [endDate, setEndDate] = useState(new Date());
   
   function DisplayCases() {
@@ -66,7 +66,7 @@ export default function App() {
   //Calculate the color for layer according to the argument d
   function ColorCases(d, max) {
     let num = max/8;
-    return d > max ? '#800026' :
+    return d >= max ? '#800026' :
            d > (num + num + num + num + num + num)  ? '#BD0026' :
            d > (num + num + num + num + num)  ? '#E31A1C' :
            d > (num + num + num + num)  ? '#FC4E2A' :
@@ -78,7 +78,7 @@ export default function App() {
   function ColorRecoveries(d, max) {
     let num = max/8;
     return d > max ? '#005824' :
-           d > (num + num + num + num + num + num + num)  ? '#238b45' :
+           d > (num + num + num + num + num + num)  ? '#238b45' :
            d > (num + num + num + num + num)  ? '#41ae76' :
            d > (num + num + num + num)  ? '#66c2a4' :
            d > (num + num + num)  ? '#99d8c9' :
@@ -89,7 +89,7 @@ export default function App() {
   function ColorDeaths(d, max) {
     let num = max/8;
     return d > max ? '#99000d' :
-           d > (num + num + num + num + num + num + num)  ? '#cb181d' :
+           d > (num + num + num + num + num + num)  ? '#cb181d' :
            d > (num + num + num + num + num)   ? '#ef3b2c' :
            d > (num + num + num + num)  ? '#fb6a4a' :
            d > (num + num + num)   ? '#fc9272' :
@@ -200,6 +200,12 @@ export default function App() {
       zoom: zoom
     });
 
+    // disable map rotation using right click + drag
+    map.dragRotate.disable();
+
+    // disable map rotation using touch rotation gesture
+    map.touchZoomRotate.disableRotation();
+
     
     map.on('load', async () => {
 
@@ -259,7 +265,7 @@ export default function App() {
             'layout': {},
             'paint': {
               'fill-color': color, 
-              'fill-opacity': 0.5
+              'fill-opacity': 1
               }
           });
 
@@ -270,8 +276,8 @@ export default function App() {
             'source': source,
             'layout': {},
             'paint': {
-              'line-color': '#000',
-              'line-width': 2
+              'line-color': 'black',
+              'line-width': 2,
             }
           });
 
@@ -292,6 +298,7 @@ export default function App() {
     map.on('mousemove', (event) => {
       const county = map.queryRenderedFeatures(event.point);
       const display = ['source', 'layer', 'properties',];
+      console.log("id: " +  event.features);
 
       const displayFeatures = county.map((feat) => {
         const displayFeat = {};
