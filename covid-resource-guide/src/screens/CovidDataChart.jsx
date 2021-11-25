3e23858444226131a3e125520bb4a79c24dcb0fa
shail7777 <parekhshail7@gmail.com>
2021-11-24 21:54:57 -0500

    Heatmap, Charts and testing locations pages works

---
+++ covid-resource-guide/src/screens/CovidDataChart.jsx
@@ -0,0 +1,125 @@
+import { Line } from "react-chartjs-2";
+import React, { useState, useEffect} from 'react';
+
+
+function CovidDataChart(props){
+  const startDate = props.startDate;
+  const endDate = props.endDate;
+  const metrics = props.metrics;
+  const fips = props.fips;
+  const countyName = props.countyName;
+  const [rows, setRows] = useState([]);
+  useEffect(() => {
+    let params = `/'${startDate}'&'${endDate}'&('${fips}')`
+    fetch("http://localhost:8080/rest/metrics/CovidData/chart_data" + params)
+      .then(response => response.json())
+      .then(
+        (json_string) => {
+          let json_data = JSON.parse(json_string);
+          let data_rows = json_data.DATA;
+          setRows(data_rows);
+        }
+      )
+  }, [startDate, endDate, fips]);
+
+  var datasets = [
+    {
+      label: "Daily Cases",
+      data: rows.DAILY_CASES,
+      backgroundColor: "rgb(255, 99, 132)",
+      borderColor: "rgba(255, 99, 132, 0.2)",
+      fill: false,
+      lineTension: 0, // Disables Bezier curves
+    },
+    {
+      label: "Total Cases",
+      data: rows.TOTAL_CASES,
+      backgroundColor: "rgb(255, 99, 132)",
+      borderColor: "rgba(255, 99, 132, 0.2)",
+      fill: false,
+      lineTension: 0, // Disables Bezier curves
+    },
+    {
+      label: "Daily Deaths",
+      data: rows.DAILY_DEATHS,
+      backgroundColor: "rgb(25, 25, 25)",
+      borderColor: "rgba(25, 25, 25, 0.2)",
+      fill: false,
+      lineTension: 0, // Disables Bezier curves
+    },
+    {
+      label: "Total Deaths",
+      data: rows.TOTAL_DEATHS,
+      backgroundColor: "rgb(25, 25, 25)",
+      borderColor: "rgba(25, 25, 25, 0.2)",
+      fill: false,
+      lineTension: 0, // Disables Bezier curves
+    },
+    {
+      label: "Daily Recoveries",
+      data: rows.DAILY_RECOVERIES,
+      backgroundColor: "rgb(132, 99, 255)",
+      borderColor: "rgba(132, 99, 255, 0.2)",
+      fill: false,
+      lineTension: 0, // Disables Bezier curves
+    },
+    {
+      label: "Total Recoveries",
+      data: rows.TOTAL_RECOVERIES,
+      backgroundColor: "rgb(132, 99, 255)",
+      borderColor: "rgba(132, 99, 255, 0.2)",
+      fill: false,
+      lineTension: 0, // Disables Bezier curves
+    },
+  ]
+
+  var data = {
+      labels: rows.DATE,
+      datasets : []
+  };
+  const legend = {
+    display: true,
+    position: "bottom",
+    labels: {
+      fontColor: "#323130",
+      fontSize: 14
+    }
+  };
+  var options = {
+    scales: {
+      xAxes: [
+        {
+          type: "time",
+          time: {
+            unit: "day"
+          }
+        },
+
+      ],
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero:true,
+            min: 0,
+          }
+        }
+      ]
+    },
+    title:{
+      display:true,
+      text: countyName
+    },
+  };
+  // Map Selected metrics to their datasets, put in graph
+  for(let i = 0; i < metrics.length; i++){
+    let metric = metrics[i];
+    let metric_id = metric.id;
+    let dataset = datasets[metric_id];
+    data.datasets.push(dataset);
+  }
+
+
+  return <Line data={data} legend={legend} options={options} />;
+
+}
+export default CovidDataChart;
---
+++ covid-resource-guide/src/screens/ListRecoveries.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './Map.css';
+
+export function ListRecoveries(props){
+    var num = Math.round(props.max/8);
+    return (
+    <div className="map-overlay">
+        <tr>
+            <p className="color-box" style={{backgroundColor: "rgb(247, 252, 253)" }}></p> <p className="data">0 - {num}</p>
+        </tr>
+        <br/><br/>
+        <tr> 
+            <p className="color-box" style={{backgroundColor: "rgb(229, 245, 249)"}}></p> <p className="data">{num} - {num + num}</p>
+        </tr>
+        <br/><br/>
+        <tr> 
+            <p className="color-box" style={{backgroundColor: "rgb(204, 236, 230)"}}></p> <p className="data">{num + num} - {num + num + num}</p>
+        </tr>
+        <br/><br/>
+        <tr> 
+            <p className="color-box" style={{backgroundColor: "rgb(153, 216, 201)"}}></p> <p className="data">{num + num + num} - {num + num + num + num}</p>
+        </tr>
+        <br/><br/>
+        <tr> 
+            <p className="color-box" style={{backgroundColor: "rgb(102, 194, 164)"}}></p> <p className="data">{num + num + num + num} - {num + num + num + num + num}</p>
+        </tr>
+        <br/><br/>
+        <tr> 
+            <p className="color-box" style={{backgroundColor: "rgb(65, 174, 118)"}}></p> <p className="data">{num + num + num + num + num} - {num + num + num + num + num + num}</p>
+        </tr>
+        <br/><br/>
+        <tr> 
+            <p className="color-box" style={{backgroundColor: "rgb(35, 139, 69)"}}></p> <p className="data">{num + num + num + num + num + num} - {props.max}</p>
+        </tr>
+        <br/><br/>
+        <tr> 
+            <p className="color-box" style={{backgroundColor: "rgb(0, 88, 36)"}}></p> <p className="data">{props.max}+</p>
+        </tr>
+    </div>
+    )
+}
